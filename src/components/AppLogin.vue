1673d64f028c5a7d76cf19c33991f4200a7c4d31
Diego C <diego_032594@outlook.es>
2018-09-18 01:39:44 -0500

    Cambio AppLogin
    Cambio en el markup y el css

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,49 +1,47 @@
 <template>
-    <div class="col-xl-12 fondo">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="my-container text_whitte contenedor_bienvenidos">
-                    <p class="titulo_login">Bienvenid@s</p>
+  <div class="applogin_contenedor"> <!--- Contenido -->
+    <div class="applogin_col_prin">
+      <div class="applogin_col_prin-cuadro">
+        <p class="applogin_titulo">Bienvenid@s</p>
 
-                    <p class="appLogin_p">Esta es tu plataforma de transformación, creación y progreso.<br>
-                    <span>¿Que meta extraordinario vas a cumplir hoy? </span></p>
-                    <form v-on:submit.prevent="noop">
-                        <input class="input_login" type="text" v-model="email" placeholder="Codigo de Usuario">
-                        <a href="" class="a_appLoggin">Otras formas de ingreso</a>
+        <p class="applogin_p">Esta es tu plataforma de transformación, creación y progreso.<br>
+        <span class="resaltador">¿Que meta extraordinaria vas a cumplir hoy?</span></p>
+        <form v-on:submit.prevent="noop">
+          <input class="applogin_input_login" type="text" v-model="email" placeholder="Codigo de Usuario">
+          <a href="" class="applogin_a">Otras formas de ingreso</a>
 
-                        <input class="input_login" type="password" v-model="password" v-on:keyup.enter="login" placeholder="Pín de Elite">
-                        <a href="" class="a_appLoggin">Olvide mi contraseña</a>
+          <input class="applogin_input_login" type="password" v-model="password" v-on:keyup.enter="login" placeholder="Pín de Elite">
+          <a href="" class="applogin_a">Olvide mi contraseña</a>
 
-                        <p class="appLogin_p" style="text-transform: uppercase; border-bottom:1px solid #FFE045;"><span>Ir directo a:</span></p>
+          <p class="applogin_p resaltador" style="text-transform: uppercase; border-bottom:1px solid #FFE045;">Ir directo a:</p>
 
-                        <div style="width: 95%;">
-                            <button type="button" v-on:click="login" class="btn_login">Inicio</button>
-                            <button type="button" v-on:click="login" class="btn_login_c">Comunidad</button>
-                            <button type="button" v-on:click="login" class="btn_login_c">Negocios</button>
-                        </div>
-                    </form>
-                </div>
+          <div class="applogin_btn_div">
+            <button type="button" v-on:click="login" class="applogin_btn_login">Inicio</button>
+            <button type="button" v-on:click="login" class="applogin_btn_login applogin_btn_login_gris">Comunidad</button>
+            <button type="button" v-on:click="login" class="applogin_btn_login applogin_btn_login_gris">Negocios</button>
+          </div>
+        </form>
+      </div>
 
-                <div class="my-container text_whitte contenedor_bienvenidos">
-                    <p class="titulo_login">¿Aún no eres parte?</p>
+      <div class="applogin_col_prin-cuadro">
+        <p class="applogin_titulo">¿Aún no eres parte?</p>
 
-                    <p class="appLogin_p">Somos una comunidad comprometida con crear un mundo más amoroso, justo y poderoso.<br>
-                    <span>¿Listo para crear metas extraordínarias</span></p>
+        <p class="applogin_p">Somos una comunidad comprometida con crear un mundo más amoroso, justo y poderoso.<br>
+        <span class="resaltador">¿Listo para crear metas extraordínarias</span></p>
 
-                    <div style="width: 95%;">
-                    <button type="button" class="btn_login">Iniciar mi Entrenamiento</button>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="col-md-7 text_whitte">
-                <span class="titulo_login" style="text-weight:500;">Este es un mensaje muy poderoso de invitación a ser parte de la comunidad más
-                poderosa del mundo desde el <span class="resaltador">amor</span> y el <span class="resaltador">compromiso</span></span>.
-            </div>
+        <div class="applogin_btn_div">
+          <button type="button" class="applogin_btn_login">Iniciar mi Entrenamiento</button>
         </div>
+      </div>
 
     </div>
+
+    <div class="applogin_col_sec">
+      <p class="applogin_titulo">Este es un mensaje muy poderoso de invitación a ser parte de la comunidad más
+      poderosa del mundo desde el <span class="resaltador">amor</span> y el <span class="resaltador">compromiso</span></p>.
+    </div>
+
+  </div> <!--- .\ Contenido -->
 </template>
 
 <style>
@@ -52,128 +50,115 @@
 
 body{
     font-family: 'Source Sans Pro', Arial, sans-serif;
-    background: no-repeat url("../assets/pexels-photo-450035.jpeg");
+    background: no-repeat url("../assets/pexels-photo-450035.jpeg") center;
 }
 
-.fondo{
-    background: no-repeat url(/static/img/pexels-photo-450035.ee06817.jpeg);
-}
-
-.text_whitte{
+.applogin_contenedor {
     color: #FFF;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 }
 
-.input_login {
-    color: #FFF!important;
-    border-radius: 400px!important;
-    border: 1px solid #4d4d4d!important;
-    background: #4d4d4d!important;
-    padding: 0 0 0 20px!important;
-    height: 35px!important;
-    width: 92%!important;
-    margin: 14px 0!important;
+.applogin_col_prin {
+  margin: 20px;
+  max-width: 430px;
 }
 
-.input_login:focus, .input_login:active {
-    border: #FBB829!important;
-    box-shadow: 0 1px 0 0 #FBB829!important;
-}
-
-.contenedor_bienvenidos{
-    margin: 15px auto;
-    padding: 20px 40px;
-    width: 100%;
+.applogin_col_prin .applogin_col_prin-cuadro {
+    margin: 0 0 20px 0;
+    padding: 20px;
     background: rgba(26 , 26 , 26 , 0.8);
-    border: 1px solid #AAA;
+    border: 2px solid #AAA;
 }
 
-.my-container div {
-    margin: 0 auto;
-    text-align: center;
+.applogin_col_prin .applogin_col_prin-cuadro:last-child {
+    margin: 0;
 }
 
-.col-md-4 {
-    max-width: 27.333333%;
-    margin: 0 0 0 15px;
-    float: left;
-    overflow: auto;
+.applogin_col_sec {
+  margin: 20px;
+  max-width: 800px;
 }
 
-.btn_login {
+.applogin_btn_div {
+  width: 90%;
+  margin: -10px auto 0 auto;
+  text-align: center;
+}
+
+.applogin_input_login {
+    color: #FFF;
+    border-radius: 400px !important;
+    border: 1px solid #4d4d4d !important;
+    background: #4d4d4d !important;
+    padding: 0 0 0 20px !important;
+    margin: 6px auto !important;
+    height: 35px !important;
+    width: 92% !important;
+}
+
+.applogin_input_login:focus,
+.applogin_input_login:active {
+    border: #FBB829 !important;
+    box-shadow: 0 1px 0 0 #FBB829 !important;
+}
+
+.applogin_btn_login {
     border-radius: 400px 400px;
     background: #FBB829;
     font-size: 16px;
     font-weight: 800;
     color: #1A1A1A;
-    border: 0!important;
-    padding: 6px 18px!important;
-    margin: 7px 5px!important;
+    border: none !important;
+    padding: 6px 18px !important;
+    margin: 7px 5px !important;
 }
 
-.btn_login:hover, .btn_login:active, .btn_login:focus{
-    background: #f7931e!important;
+.applogin_btn_login:hover,
+.applogin_btn_login:active,
+.applogin_btn_login:focus{
+    background: #f7931e !important;
 }
 
-.btn_login_c {
-    border-radius: 400px 400px;
+.applogin_btn_login_gris {
     background: #B3B3B3;
-    font-size: 16px;
-    font-weight: 800;
-    color: #1A1A1A;
-    border: 0!important;
-    padding: 6px 18px!important;
-    margin: 7px 5px!important;
 }
 
-.btn_login_c:hover, .btn_login_c:active, .btn_login_c:focus{
-    background: #797B7F!important;
+.applogin_btn_login_gris:hover,
+.applogin_btn_login_gris:active,
+.applogin_btn_login_gris:focus{
+    background: #797B7F !important;
 }
 
-.a_appLoggin, .a_appLoggin:active, .a_appLoggin:visited {
-    font-size: 16px;
-    text-decoration: underline;
-}
-
-.a_appLoggin {
+.applogin_a, .applogin_a:active, .applogin_a:visited {
     font-size: 16px;
     text-decoration: underline;
     color:#FFF;
+    margin: 4px auto 4px 20px;
 }
 
-.a_appLoggin:hover {
+.applogin_a:hover {
     color:#FFF;
     text-decoration: none;
 }
 
-.appLogin_p {
+.applogin_p {
     font-size: 18px;
     text-align: center;
-    margin: 20px 0;
+    margin: 14px 0;
 }
 
-.appLogin_p span {
-    color: #FFE045;
+.resaltador {
+    color: #FBB829;
 }
 
-.titulo_login{
+.applogin_titulo {
     font-size: 32px;
     font-family: 'Fjalla One', Arial, sans-serif;
     text-align: center;
     margin: 5px auto;
     text-transform: uppercase;
-}
-
-.col-md-7 {
-    /** No style **/
-    float: right;
-    width: 60%;
-    margin: 0 15px 0 0;
-    padding: 60px;
-}
-
-.resaltador{
-    color: #FBB829;
-    font-family: 'Fjalla One', Arial, sans-serif;
 }
 
 
